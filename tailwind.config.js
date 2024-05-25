2390b75369006c32accb8b4ec6d735bfd3a8360d
DevBasitali <rajaalix2022@gmail.com>
2024-05-25 20:14:31 +0500

    Saturday"25-May-2024"

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#577B8D",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
